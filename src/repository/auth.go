9bf498f87a3f6bb3b8bc4a036c1b9bb72ba56044
Krzysztof Rozbicki <rozkrzys@wp.pl>
2024-09-28 16:46:20 +0200

    Add to CORS

--- src/repository/auth.go
+++ src/repository/auth.go
@@ -64,7 +64,7 @@ func CookieAuth(ctx *gin.Context) {
 
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "https://hackarena.pl")
+		c.Writer.Header().Set("Access-Control-Allow-Origins", "https://hackarena.pl, https://test.hackarena.pl")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With,Hack-Arena-API-Key")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
